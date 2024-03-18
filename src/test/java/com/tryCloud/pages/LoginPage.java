7c7d7b00a32ff4c05c0ec17d9dd325b144df21fb
fatihyalcin1 <yalcinn.fatih006@gmail.com>
2024-03-18 22:45:11 +0300

    create project

--- src/test/java/com/tryCloud/pages/LoginPage.java
+++ src/test/java/com/tryCloud/pages/LoginPage.java
@@ -7,11 +7,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class LoginPage {
+public class LoginPage extends BasePage{
 
-    public LoginPage(){
-        PageFactory.initElements(Driver.getDriver(), this);
-    }
+
 
     @FindBy(id="user")
     public WebElement userName;
